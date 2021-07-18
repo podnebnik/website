e736402aa8f78ed328784859704016c95134cbdc
Joh Dokler <joh.dokler@gmail.com>
2021-07-18 09:54:59 +0200

    Disable admin sidebar

--- podnebnik/urls.py
+++ podnebnik/urls.py
@@ -8,6 +8,8 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
+admin.site.enable_nav_sidebar = False
+
 urlpatterns = [
     path('cms/', include(wagtailadmin_urls)),
     path('admin/', admin.site.urls),
