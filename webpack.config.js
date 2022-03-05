384ab6849199d60c6fa0612c18a32fbbe7a23744
Maja Brajnik <maja.brajnik@gmail.com>
2022-03-05 12:37:00 +0100

    Upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = (env, argv) => {
             },
             proxy: {
                 "/": {
-                    target: "http://localhost:8000",
+                    target: "http://127.0.0.1:8000",
                     changeOrigin: true
                 }
             },
