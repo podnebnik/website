ca35707b83e8d5ca5a7acb7a2e0048924667ad3b
Maja Brajnik <maja.brajnik@gmail.com>
2022-03-05 12:43:44 +0100

    Remove language selector

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,14 +1,6 @@
 import feather from "feather-icons"
 import "../styles/main.sass"
 
-// Language selector
-
-let languageSelector = document.getElementById("language-selector")
-
-languageSelector.addEventListener('click', function () {
-    languageSelector.classList.toggle("is-active")
-})
-
 // Slide menu
 
 let menuToggle = document.getElementById("menuToggle")
