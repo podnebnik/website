753611cf8752c2fedac7cb0eb4bf29515373da28
Joh Dokler <joh.dokler@gmail.com>
2022-03-06 10:18:30 +0100

    Fix django/webpack URLs

--- podnebnik/settings/develop.py
+++ podnebnik/settings/develop.py
@@ -6,6 +6,7 @@ from .base import BASE_DIR
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8080']
 
 SECRET_KEY = '%#orw69hm#755h)i2gq=wyds02+82rh5j*mjh2r7p12lesac=_'
 
