ad35b65f4bef03a14816f5d6b4a95e2457f73801
Joh Dokler <joh.dokler@gmail.com>
2023-02-05 17:43:59 +0100

    Create a datasette metadata

--- tasks.py
+++ tasks.py
@@ -1,6 +1,8 @@
 from invoke import task
 
 import glob
+import json
+import shutil
 from pathlib import Path
 from frictionless import Package
 
@@ -32,6 +34,7 @@ def get_datapackages():
 
 @task
 def validate(c):
+    '''Validate available data packages.'''
     print('Validating data packages:\n')
     invalid_package_paths = []
     for path in get_datapackage_paths():
@@ -56,25 +59,79 @@ def validate(c):
         exit(1)
 
 
+def none_if_empty(value):
+    if value:
+        return value
+    else:
+        return None
+
+
 @task
 def create_databases(c):
+    '''Create sqlite database, import resources data and generate datasette metadata.'''
     validate(c)
+
+    # Reset the sqlite databases directory
+    shutil.rmtree(SQLITE_DIR, ignore_errors=True)
+    SQLITE_DIR.mkdir(parents=True, exist_ok=True)
+
+    # Datasette metadata
+    metadata = {
+        'title': 'Podnebnik',
+        'description': 'Podatki o podnebnih spremembah.',
+        # 'license': None,
+        # 'license_url': None,
+        # 'source': None,
+        # 'source_url': None,
+        'databases': {}
+    }
+
+    # Create a new database for each data package
     for package_path in get_datapackage_paths():
         package = Package(package_path)
         database = Path(f'{SQLITE_DIR / package.name}.db')
+
         print(f'\nImporting data package {package.name}:')
-        SQLITE_DIR.mkdir(parents=True, exist_ok=True)
-        database.unlink(missing_ok=True)
+
+        # Package metadata
+        metadata['databases'][package.name] = {
+            'title': package.title,
+            'description': package.description,
+            'license': none_if_empty(', '.join([license.title for license in package.licenses if hasattr(license, 'title')])),
+            'license_url': none_if_empty(', '.join([license.path for license in package.licenses if hasattr(license, 'path')])),
+            # 'source': None,
+            # 'source_url': None,
+            'tables': {},
+        }
+
+        # Import resources
         for resource in package.resources:
             if resource.format == 'csv':
+                # Import the resource if it is a CSV file
                 print(f'    Importing resource {resource.name}, {resource.format} @ {resource.path}')
+                metadata['databases'][package.name]['tables'][resource.name] = {
+                    'title': resource.title,
+                    'description': resource.description,
+                    'license': none_if_empty(', '.join([license.title for license in resource.licenses if 'title' in license])),
+                    'license_url': none_if_empty(', '.join([license.path for license in resource.licenses if 'path' in license])),
+                    # 'source': None,
+                    # 'source_url': None,
+                }
+                if resource.schema.fields:
+                    metadata['databases'][package.name]['tables'][resource.name]['columns'] = dict([(field.name, field.title) for field in resource.schema.fields if hasattr(field, 'title')])
+                    metadata['databases'][package.name]['tables'][resource.name]['units'] = dict([(field.name, field.unit) for field in resource.schema.fields if hasattr(field, 'unit')])
             else:
                 print(f'    Skipping resource {resource.name}, {resource.format} @ {resource.path}')
             c.run(f'sqlite-utils insert {database} {resource.name} {DATASETS_DIR / package_path.parent /resource.path} --csv --detect-types --silent')
 
+    # Write metadata
+    with open(SQLITE_DIR / 'metadata.json', 'w') as fo:
+        json.dump(metadata, fo, indent=4)
+
 
 @task
 def datasette(c):
+    '''Start datasette server.'''
     create_databases(c)
     print('\nStarting Datasette server...\n')
-    c.run(f'datasette serve {SQLITE_DIR} --port 8001 --cors')
+    c.run(f'datasette serve {SQLITE_DIR} --metadata {SQLITE_DIR}/metadata.json --port 8001 --cors')
