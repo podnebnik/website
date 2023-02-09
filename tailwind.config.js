50e0e433fdbf4cb00f06c7ad30789c8a1c289949
Joh Dokler <joh.dokler@gmail.com>
2023-02-09 14:09:52 +0100

    Add tailwind and image processing

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+        "./pages/**/*.{html,md,js}",
+        "./scripts/**/*.{html,js,jsx,ts,tsx}",
+    ],
+    plugins: [
+        require("@tailwindcss/typography"),
+    ],
+}
