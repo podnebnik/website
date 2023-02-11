404bb42e5dd44a2f30702d4e31ae0c3d7963e245
Joh Dokler <joh.dokler@gmail.com>
2023-02-11 02:03:45 +0100

    Add Fantomas and VS Code extension recommendations

--- code/Chart.fs
+++ code/Chart.fs
@@ -2,17 +2,15 @@ module Chart
 
 open Fable.Core
 open Fable.Core.JsInterop
-open Elmish.Solid
 
-let private data : obj = importDefault "./data"
+let private data: obj = importDefault "./data"
 
 let private config =
     {| title = {| text = "Fruit Consumption in F#" |}
        chart = {| ``type`` = "bar" |}
        xAxis = {| categories = [ "Apples", "Bananas", "Oranges" ] |}
        yAxis = {| title = {| text = "Fruit eaten" |} |}
-       series = data
-    |}
+       series = data |}
 
 let private config1 =
     {| config with
@@ -20,17 +18,21 @@ let private config1 =
 
 let private config2 =
     {| config with
-        chart = {| config.chart with ``type`` = "line" |} |}
+        chart =
+            {| config.chart with
+                ``type`` = "line" |} |}
 
 [<ImportDefault("highcharts")>]
 let private highcharts: obj = jsNative
+
+type Foo = { Bar: string; Baz: string }
 
 type private Kind =
     | Bar
     | Line
 
 [<JSX.Component>]
-let Chart (kind : string) =
+let Chart (kind: string) =
     let defaultConfig =
         match kind with
         | "bar" -> config1
@@ -40,14 +42,14 @@ let Chart (kind : string) =
     let config, setConfig = Solid.createSignal defaultConfig
 
     let chart element =
-        Solid.createEffect (fun () -> highcharts?chart (element, config()))
+        Solid.createEffect (fun () -> highcharts?chart (element, config ()))
 
     JSX.html
         $"""
     <>
         <div>
-            <button disabled={config() = config1} onClick={fun () -> setConfig(config1)}>Chart 1</button>
-            <button disabled={config() = config2} onClick={fun () -> setConfig(config2)}>Chart 2</button>
+            <button disabled={config () = config1} onClick={fun () -> setConfig (config1)}>Chart 1</button>
+            <button disabled={config () = config2} onClick={fun () -> setConfig (config2)}>Chart 2</button>
         </div>
         <div use:chart></div>
     </>
