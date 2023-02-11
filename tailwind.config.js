43b9491b18f2e798f013815840e6a89ed7efe069
Maja Brajnik <maja.brajnik@gmail.com>
2023-02-11 13:44:28 +0100

    WIP - layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,37 @@ module.exports = {
         "./pages/**/*.{html,md,js}",
         "./code/**/*.{html,js,jsx,ts,tsx}",
     ],
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#462a44',
+                'secondary': '#ffb644',
+                'tertiary': '#f3f1f0',
+                'muted': '#7a7a7a',
+            },
+            spacing: {
+                'gap': '1rem',
+                '2gap': '2rem',
+                '3gap': '3rem',
+                '4gap': '4rem',
+                '5gap': '5rem',
+                '6gap': '6rem',
+                '7gap': '7rem',
+                '8gap': '8rem',
+            },
+            fontFamily: {
+                sans: ['Bitter', 'Arial', 'Helvetica', 'sans-serif'],
+                serif: ['IBM Plex Sans', 'serif'],
+            },
+            typography: (theme) => ({
+                DEFAULT: {
+                    css: {
+                        color: '#212529',
+                    },
+                },
+            }),
+        },
+    },
     plugins: [
         require("@tailwindcss/typography"),
     ],
